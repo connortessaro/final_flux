9279717f077e1150d4be321d98692817739747b7
Connor Tessaro <116526628+connortessaro@users.noreply.github.com>
2024-06-15 19:46:37 -0700

    initial commit

---
+++ data.js
@@ -0,0 +1,48 @@
+var threads = [
+    {
+        id: 1,
+        title: "Thread 1",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "nicholas",
+                date: Date.now(),
+                content: "Hey to u to"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Thread 2",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "nicholas",
+                date: Date.now(),
+                content: "Hey to u to"
+            }
+        ]
+    }
+]
+
+var threads;
+if (localStorage && localStorage.getItem('threads')) {
+    threads = JSON.parse(localStorage.getItem('threads'));
+} else {
+    threads = defaultThreads;
+    localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
